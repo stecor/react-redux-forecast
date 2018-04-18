a16631a0a5b324e662e77b4571ca0fe7f82c4d99
Stefano Corra <stefano.corra@hotmail.com>
2018-04-17 23:16:08 -0400

    maps

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -8,14 +8,17 @@ class WeatherList extends Component{
   renderWeather(cityData,index){
 
     const name = cityData.city.name;
-    const temps = cityData.list.map(weather => weather.main.temp);
+    const temps = cityData.list.map(weather => weather.main.temp - 273.15);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
+
 
     return(
       <tr key={index}>
         <td>{name}</td>
-        <td>
-          <Chart data={temps} color="orange" />
-        </td>
+        <td><Chart   data={temps} color="orange" units="°C" /></td>
+        <td><Chart   data={pressures} color="green" units="hPa" /></td>
+        <td><Chart   data={humidities} color="black" units="%" /></td>
       </tr>
     );
   }
@@ -27,9 +30,9 @@ class WeatherList extends Component{
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temperature (°C)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
         <tbody>
